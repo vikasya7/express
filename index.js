6754d1f2a29c74ce7d11ca4e80d7cbe3c0095f62
vikasya7 <vikasyaa19@gmail.com>
2025-12-30 12:25:52 +0530

    implement logging with Winston and Morgan; add logger.js and app.log

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import 'dotenv/config'
+import logger from "./logger.js";
+import morgan from "morgan";
+
 import express from "express"
 
 const app = express()
@@ -8,6 +11,22 @@ const port =process.env.PORT || 3000
 
 app.use(express.json())
 
+const morganFormat = ":method :url :status :response-time ms";
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 let teaData = []
 let nextId=1
 
